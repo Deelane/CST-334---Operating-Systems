f08c885de35ae6f394adcc8f5cca2630dbe4e861
Dae Dae <55857290+Deelane@users.noreply.github.com>
2021-12-24 20:07:04 -0800

    Add files via upload

---
+++ week2/lab/step1.c
@@ -0,0 +1,31 @@
+/*Sample C program for Lab assignment 1*/
+#include <stdio.h> /* printf, stderr */
+#include <sys/types.h> /* pid_t */
+#include <unistd.h> /* fork */
+#include <stdlib.h> /* atoi */
+#include <errno.h> /* errno */
+/* main function with command-line arguments to pass */
+int main(int argc, char *argv[]) {
+  pid_t pid;
+  int i, n = atoi(argv[1]); // n microseconds to input from keyboard for delay
+  printf("\n Before forking.\n");
+  pid = fork();
+  if (pid == -1) {
+    fprintf(stderr, "can't fork, error %d\n", errno);
+  }
+  if (pid){
+    // Parent process
+    for (i=0;i<100;i++) {
+      printf("\t \t \t Parent Process %d \n",i);
+      usleep(n);
+    }
+  }
+  else{
+    // Child process
+    for (i=0;i<100;i++) {
+      printf("Child process %d\n",i);
+      usleep(n);
+    }
+  }
+  return 0;
+}
